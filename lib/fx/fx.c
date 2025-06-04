81334375ff4fc1c4bf2eebdf5d97f4141873dace
Shinoa-Fores <btcinfo@sdf.org>
2025-06-04 17:40:02 -0500

    Fix for FX rates window

--- lib/fx/fx.c
+++ lib/fx/fx.c
@@ -17,26 +17,12 @@
 
 static int verbose;
 
-static const char *url = "http://blockchain.info/ticker";
-
-/*
- * Other usable URLs:
- *      http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast
- *      http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast?pretty
- *      http://blockchain.info/ticker
- *      http://api.bitcoincharts.com/v1/markets.json
- */
-
-/*
- * inspired from:
- *       http://curl.haxx.se/libcurl/c/multi-single.html
- */
+static const char *url = "https://blockchain.info/ticker";
 
 struct fx_state {
    CURL         *http_handle;
    CURLM        *multi_handle;
-   struct buff *buf;
-
+   struct buff  *buf;
    bool         timeCb;
    int          fd_rd;
    int          fd_wr;
@@ -45,7 +31,6 @@ struct fx_state {
 static struct fx_state fx;
 
 static void fx_api_register_poll(void);
-
 
 /*
  *---------------------------------------------------------------------------
@@ -58,18 +43,23 @@ static void fx_api_register_poll(void);
 static void
 fx_cleanup(void)
 {
-   buff_free(fx.buf);
-   fx.buf = NULL;
-
-   curl_multi_remove_handle(fx.multi_handle, fx.http_handle);
-
-   curl_easy_cleanup(fx.http_handle);
-   fx.http_handle = NULL;
-
-   curl_multi_cleanup(fx.multi_handle);
-   fx.multi_handle = NULL;
+   if (fx.buf) {
+      buff_free(fx.buf);
+      fx.buf = NULL;
+   }
+   if (fx.http_handle) {
+      curl_multi_remove_handle(fx.multi_handle, fx.http_handle);
+      curl_easy_cleanup(fx.http_handle);
+      fx.http_handle = NULL;
+   }
+   if (fx.multi_handle) {
+      curl_multi_cleanup(fx.multi_handle);
+      fx.multi_handle = NULL;
+   }
+   fx.fd_rd = -1;
+   fx.fd_wr = -1;
+   fx.timeCb = 0;
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -85,41 +75,30 @@ fx_api_parse_json_entry(cJSON           *root,
                         struct bitcui_fx *fx_pair,
                         int             *i)
 {
-   cJSON *item;
-   cJSON *symbol;
-   cJSON *last;
-
-   item = cJSON_GetObjectItem(root, name);
+   cJSON *item = cJSON_GetObjectItem(root, name);
    if (item == NULL) {
       Log(LGPFX" failed to find entry '%s'.\n", name);
       return;
    }
-   symbol = cJSON_GetObjectItem(item, "symbol");
-   last   = cJSON_GetObjectItem(item, "last");
+   cJSON *symbol = cJSON_GetObjectItem(item, "symbol");
+   cJSON *last = cJSON_GetObjectItem(item, "last");
    if (symbol == NULL || last == NULL) {
-      Log(LGPFX" failed to parse json entry\n");
+      Log(LGPFX" failed to parse json entry for %s\n", name);
+      return;
+   }
+   if (last->type != cJSON_Number || symbol->type != cJSON_String) {
+      Log(LGPFX" invalid types for %s: last=%d, symbol=%d\n",
+          name, last->type, symbol->type);
       return;
    }
 
-   ASSERT(last->type == cJSON_Number);
-   ASSERT(symbol->type == cJSON_String);
-
-   fx_pair[*i].name   = safe_strdup(name);
-   /*
-    * We might need to use wcsdup() here eventually.
-    */
+   fx_pair[*i].name = safe_strdup(name);
    fx_pair[*i].symbol = safe_strdup(symbol->valuestring);
-   fx_pair[*i].value  = last->valuedouble;
+   fx_pair[*i].value = last->valuedouble;
 
-   if (strcmp(item->string, "USD") == 0 ||
-       strcmp(item->string, "EUR") == 0 ||
-       strcmp(item->string, "CHF") == 0) {
-      LOG(1, (LGPFX" -- %s : %13.7f\n",
-          item->string, last->valuedouble));
-   }
+   LOG(1, (LGPFX" -- %s : %13.7f %s\n", name, fx_pair[*i].value, fx_pair[*i].symbol));
    (*i)++;
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -138,17 +117,29 @@ fx_api_parse_json(void)
    int n;
 
    root = cJSON_Parse(buff_base(fx.buf));
-   buff_free(fx.buf);
-   fx.buf = NULL;
-
    if (root == NULL) {
-      Log(LGPFX" failed to parse JSON\n");
+      Log(LGPFX" failed to parse JSON: %s\n", (char *)buff_base(fx.buf));
+      buff_free(fx.buf);
+      fx.buf = NULL;
+      btcui->fx_pairs = NULL;
+      btcui->fx_num = 0;
+      bitcui_fx_update();
       return;
    }
 
    n = 0;
    for (item = root->child; item != NULL; item = item->next) {
       n++;
+   }
+   if (n == 0) {
+      Log(LGPFX" no currency pairs found in JSON\n");
+      cJSON_Delete(root);
+      buff_free(fx.buf);
+      fx.buf = NULL;
+      btcui->fx_pairs = NULL;
+      btcui->fx_num = 0;
+      bitcui_fx_update();
+      return;
    }
 
    fx_pairs = safe_malloc(n * sizeof *fx_pairs);
@@ -157,7 +148,6 @@ fx_api_parse_json(void)
    fx_api_parse_json_entry(root, "USD", fx_pairs, &n);
    fx_api_parse_json_entry(root, "EUR", fx_pairs, &n);
    fx_api_parse_json_entry(root, "CHF", fx_pairs, &n);
-
    fx_api_parse_json_entry(root, "GBP", fx_pairs, &n);
    fx_api_parse_json_entry(root, "AUD", fx_pairs, &n);
    fx_api_parse_json_entry(root, "CAD", fx_pairs, &n);
@@ -171,11 +161,16 @@ fx_api_parse_json(void)
    fx_api_parse_json_entry(root, "SGD", fx_pairs, &n);
    fx_api_parse_json_entry(root, "HKD", fx_pairs, &n);
    fx_api_parse_json_entry(root, "THB", fx_pairs, &n);
-   cJSON_Delete(root);
 
-   /*
-    * Now update the UI elements.
-    */
+   cJSON_Delete(root);
+   buff_free(fx.buf);
+   fx.buf = NULL;
+
+   if (n == 0) {
+      Log(LGPFX" no valid currency pairs parsed\n");
+      free(fx_pairs);
+      fx_pairs = NULL;
+   }
 
    if (btcui->fx_provider == NULL) {
       btcui->fx_provider = safe_strdup("blockchain.info");
@@ -188,7 +183,6 @@ fx_api_parse_json(void)
 
    bitcui_fx_update();
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -205,6 +199,9 @@ fx_api_poll_cb(void *clientData)
    uintptr_t v = (uintptr_t)clientData;
    CURLMcode err;
 
+   LOG(1, (LGPFX" poll_cb: clientData=%lu, fd_rd=%d, fd_wr=%d\n",
+           v, fx.fd_rd, fx.fd_wr));
+
    if (v == 0) {
       fx.timeCb = 0;
    } else if (v == 1) {
@@ -218,6 +215,7 @@ fx_api_poll_cb(void *clientData)
    if (err != CURLM_OK) {
       Log(LGPFX" curl_multi_perform returned %s (%d)\n",
           curl_multi_strerror(err), err);
+      fx_cleanup();
       return;
    }
 
@@ -226,9 +224,6 @@ fx_api_poll_cb(void *clientData)
       return;
    }
 
-   /*
-    * The transfer is done.
-    */
    while (TRUE) {
       int msg_left;
       CURLMsg *msg;
@@ -246,11 +241,17 @@ fx_api_poll_cb(void *clientData)
             fx_api_parse_json();
             fx_cleanup();
             return;
+         } else {
+            Log(LGPFX" curl error: %s\n", curl_easy_strerror(msg->data.result));
+            btcui->fx_pairs = NULL;
+            btcui->fx_num = 0;
+            bitcui_fx_update();
+            fx_cleanup();
+            return;
          }
       }
    }
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -263,9 +264,7 @@ fx_api_poll_cb(void *clientData)
 static void
 fx_api_register_poll(void)
 {
-   fd_set fdread;
-   fd_set fdwrite;
-   fd_set fdexcep;
+   fd_set fdread, fdwrite, fdexcep;
    int maxfd = -1;
    CURLMcode err;
    int fd;
@@ -278,14 +277,26 @@ fx_api_register_poll(void)
    if (err != CURLM_OK) {
       Log(LGPFX" curl_multi_fdset returned %s (%d)\n",
           curl_multi_strerror(err), err);
+      fx_cleanup();
       return;
+   }
+
+   if (fx.fd_rd != -1) {
+      Log(LGPFX" removing fd_rd=%d\n", fx.fd_rd);
+      poll_callback_device_remove(btcui->poll, fx.fd_rd, 1, 0, 0,
+                                 fx_api_poll_cb, (void*)(uintptr_t)1);
+      fx.fd_rd = -1;
+   }
+   if (fx.fd_wr != -1) {
+      Log(LGPFX" removing fd_wr=%d\n", fx.fd_wr);
+      poll_callback_device_remove(btcui->poll, fx.fd_wr, 0, 1, 0,
+                                 fx_api_poll_cb, (void*)(uintptr_t)2);
+      fx.fd_wr = -1;
    }
 
    if (maxfd == -1) {
       fx.timeCb = 1;
-      poll_callback_time(btcui->poll, 100 * 1000 /* 100 msec */,
-                         0 /* not permanent */,
-                         fx_api_poll_cb, NULL);
+      poll_callback_time(btcui->poll, 100 * 1000, 0, fx_api_poll_cb, NULL);
       return;
    }
 
@@ -298,21 +309,17 @@ fx_api_register_poll(void)
       }
 
       if (rd) {
-         ASSERT(fx.fd_rd == -1);
          fx.fd_rd = fd;
       }
       if (wr) {
-         ASSERT(fx.fd_wr == -1);
          fx.fd_wr = fd;
       }
 
       LOG(1, (LGPFX" registering fd=%d rd=%d wr=%d\n", fd, rd, wr));
-      poll_callback_device(btcui->poll, fd, rd, wr,
-                           0 /* not permanent */,
-                           fx_api_poll_cb, (void *)(uintptr_t)(rd ? 1 : 2));
+      poll_callback_device(btcui->poll, fd, rd, wr, 0, fx_api_poll_cb,
+                           (void *)(uintptr_t)(rd ? 1 : 2));
    }
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -336,7 +343,6 @@ fx_api_curl_write_cb(void *ptr,
    return len;
 }
 
-
 /*
  *---------------------------------------------------------------------------
  *
@@ -351,28 +357,51 @@ fx_do_update(void)
    int still_running;
    CURLMcode err;
 
+   if (fx.http_handle || fx.multi_handle) {
+      Log(LGPFX" previous API call still in progress, skipping\n");
+      return 0;
+   }
+
    fx.buf = buff_alloc();
+   if (!fx.buf) {
+      Log(LGPFX" buff_alloc failed\n");
+      return 1;
+   }
 
    fx.http_handle = curl_easy_init();
-   ASSERT(fx.http_handle);
+   if (!fx.http_handle) {
+      Log(LGPFX" curl_easy_init failed\n");
+      buff_free(fx.buf);
+      fx.buf = NULL;
+      return 1;
+   }
+
    curl_easy_setopt(fx.http_handle, CURLOPT_URL, url);
    curl_easy_setopt(fx.http_handle, CURLOPT_WRITEFUNCTION, fx_api_curl_write_cb);
    curl_easy_setopt(fx.http_handle, CURLOPT_WRITEDATA, NULL);
 
    fx.multi_handle = curl_multi_init();
+   if (!fx.multi_handle) {
+      Log(LGPFX" curl_multi_init failed\n");
+      curl_easy_cleanup(fx.http_handle);
+      fx.http_handle = NULL;
+      buff_free(fx.buf);
+      fx.buf = NULL;
+      return 1;
+   }
+
    curl_multi_add_handle(fx.multi_handle, fx.http_handle);
    err = curl_multi_perform(fx.multi_handle, &still_running);
    if (err != CURLM_OK) {
-      Log(LGPFX" curl_multi_fdset returned %s (%d)\n",
+      Log(LGPFX" curl_multi_perform returned %s (%d)\n",
           curl_multi_strerror(err), err);
+      fx_cleanup();
       return 1;
    }
 
    fx_api_register_poll();
-
    return 0;
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -388,7 +417,6 @@ fx_periodic_cb(void *clientData)
    fx_do_update();
 }
 
-
 /*
  *---------------------------------------------------------------------------
  *
@@ -402,21 +430,18 @@ fx_init(void)
 {
    fx.fd_rd = -1;
    fx.fd_wr = -1;
+   fx.timeCb = 0;
 
    Log(LGPFX" using %s\n", curl_version());
 
-   /*
-    * Update every 5min by default.
-    */
    btcui->fxPeriodMin = config_getint64(btc->config, 5, "fx.periodMin");
    ASSERT(btcui->fxPeriodMin > 0);
 
    poll_callback_time(btcui->poll, btcui->fxPeriodMin * 60 * 1000 * 1000,
-                      1 /* permanent */, fx_periodic_cb, NULL);
+                      1, fx_periodic_cb, NULL);
 
    fx_do_update();
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -432,21 +457,23 @@ fx_check_unregister_cbs(void)
    bool s;
 
    if (fx.timeCb) {
+      Log(LGPFX" removing time callback\n");
       s = poll_callback_time_remove(btcui->poll, 0, fx_api_poll_cb, NULL);
       ASSERT(s);
    }
    if (fx.fd_rd != -1) {
+      Log(LGPFX" removing fd_rd=%d\n", fx.fd_rd);
       s = poll_callback_device_remove(btcui->poll, fx.fd_rd, 1, 0, 0,
                                       fx_api_poll_cb, (void*)(uintptr_t)1);
       ASSERT(s);
    }
    if (fx.fd_wr != -1) {
+      Log(LGPFX" removing fd_wr=%d\n", fx.fd_wr);
       s = poll_callback_device_remove(btcui->poll, fx.fd_wr, 0, 1, 0,
                                       fx_api_poll_cb, (void*)(uintptr_t)2);
       ASSERT(s);
    }
 }
-
 
 /*
  *---------------------------------------------------------------------------
@@ -463,4 +490,3 @@ fx_exit(void)
    fx_check_unregister_cbs();
    fx_cleanup();
 }
-
